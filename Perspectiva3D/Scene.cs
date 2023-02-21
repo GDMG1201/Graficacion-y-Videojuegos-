e4b68528fe018631347b866d584fd789d360de49
GDMG <97765545+GDMG1201@users.noreply.github.com>
2023-02-20 19:36:57 -0600

    Implementacion clase Scene

---
+++ Perspectiva3D/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3D
+{
+    public  class Scene
+    {
+        public List<Figura> Cubes;
+        public Scene()
+        {
+            Cubes = new List<Figura>();
+        }
+    }
+}
